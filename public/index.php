9a47cc0e246dc02860565216e811db36d0bc4d9f
youssefsaad07999-star <youssefsaad07999@gmail.com>
2026-07-30 00:21:15 +0300

    Modified index.php

--- public/index.php
+++ public/index.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Artisan;
 
 define('LARAVEL_START', microtime(true));
 
@@ -17,7 +16,5 @@ require __DIR__.'/../vendor/autoload.php';
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
-
-Artisan::call('migrate', ['--force' => true]);
 
 $app->handleRequest(Request::capture());
