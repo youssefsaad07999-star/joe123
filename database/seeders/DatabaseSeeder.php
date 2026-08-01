40a481221b68b8821c2219acc2b10a1136aef20f
youssefsaad07999-star <youssefsaad07999@gmail.com>
2026-08-01 20:25:20 +0300

    Test Deployment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //
         $this->call([
             CategorySeeder::class,
             FitSeeder::class,
